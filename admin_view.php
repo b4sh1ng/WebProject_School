34938646372a2f4eef5a67e7fe170b834b601560
b4sh1ng <bashing@outlook.de>
2023-03-17 11:15:15 +0100

    waaaaa

--- admin_view.php
+++ admin_view.php
@@ -16,62 +16,59 @@
             <h3 style="margin: 5px;">Backup Liste</h3>
             <!--BackUp View hier einfügen-->
             <div class="scroll-view">
-                <table class="table table-striped table-dark table-hover table-sm rounded">
+                <table class="table table-striped table-dark table-hover table-sm rounded" id="backup_tab">
                     <thead>
                         <tr>
                             <th>#</th>
                             <th>Log Datum</th>
-                            <th>Dateigröße</th>
+                            <th>Auswahl</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>10.03.2023 10:45</td>
-                            <td>20kb</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>10.03.2023 10:45</td>
-                            <td>11kb</td>
-                        </tr>
+                        <?php
+                        $backupPath = "/home/dotto/backup/";
+                        $dataInPath = scandir($backupPath);
+                        $dataInPath = array_reverse($dataInPath);
+                        $iter = 1;
+                        foreach ($dataInPath as $data) {
+                            if ($data == "." || $data == ".." || $data == "backup.sh") {
+                                continue;
+                            }
+                            echo "<tr id='row " . $iter . "'>
+                            <td>$iter</td>
+                            <td>$data</td>
+                            <td ><input style='margin-left: 25px;' type='checkbox'/></td>
+                            </tr>";
+                            $iter = $iter + 1;
+                        }
+                        ?>
                     </tbody>
                 </table>
             </div>
             <div class="button-container">
-                <form action="admin_view.php">
+                <form action="admin_view.php" method="post">
 
-                    <input type="submit" value="Backup erstellen" name="btn_bCreate" class="btn btn-success" style="margin-left: 1em; margin-bottom: 1em;" />
-                    <input type="submit" value="Backup Vorschau" name="btn_bView" class="btn btn-primary" style="margin-left: 1em; margin-bottom: 1em;" />
-                    <input type="submit" value="Log laden" name="btn_lView" class="btn btn-warning" style="margin-left: 1em; margin-bottom: 1em;" />
+                    <input type="submit" value="Backup erstellen" name="btn_Submit" class="btn btn-success" style="margin-left: 1em; margin-bottom: 1em;" />
+                    <input type="button" value="Backup Vorschau" onclick="GetSelected()" name="btn_Submit" class="btn btn-primary" style="margin-left: 1em; margin-bottom: 1em;" />
+                    <input type="submit" value="Log laden" name="btn_Submit" class="btn btn-warning" style="margin-left: 1em; margin-bottom: 1em;" />
 
                 </form>
             </div>
         </div>
         <div class="rightDiv">
-            <!--Preview hier einfügen-->
             <div style="margin: 5px;">
                 <h1 style="margin: 5px;">Vorschau</h1>
                 <div class="scroll-view" style="margin: 3em; background-color: grey;">
                     <?php
-                    /* if(btn_bView == true && backupSelected())
-                        {
-                            loadSelectedBackupView();
-                        }
-                        elif(btn_bView == true)
-                        {
-                            loadLatedBackupView();
-                        }
-                        elif(btn_blView == true)
-                        {
-                            loadLogView();
-                        }
-                    */
-                    echo file_get_contents("/home/bash/Documents/vscode/webproj/weprori/backups/test_backup.sql");
+                    if (isset($_POST['btn_Submit'])) {
+                        shell_exec('/bin/bash /home/dotto/backup/backup.sh');
+                        echo '<script>alert("Backup erstellt!");</script>';
+                    }
                     ?>
                 </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
