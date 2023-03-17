a66c9061546fa8f86d8f9ced2f33fc2121cd2ab5
b4sh1ng <bashing@outlook.de>
2023-03-17 12:42:18 +0100

    Added loading of Backups in preview and log prev

--- admin_view.php
+++ admin_view.php
@@ -14,60 +14,74 @@
     <div class="outerDiv">
         <div class="leftDiv">
             <h3 style="margin: 5px;">Backup Liste</h3>
-            <!--BackUp View hier einfügen-->
-            <div class="scroll-view">
-                <table class="table table-striped table-dark table-hover table-sm rounded" id="backup_tab">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Log Datum</th>
-                            <th>Auswahl</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $backupPath = "/home/dotto/backup/";
-                        $dataInPath = scandir($backupPath);
-                        $dataInPath = array_reverse($dataInPath);
-                        $iter = 1;
-                        foreach ($dataInPath as $data) {
-                            if ($data == "." || $data == ".." || $data == "backup.sh") {
-                                continue;
-                            }
-                            echo "<tr id='row " . $iter . "'>
+            <form action="admin_view.php" method="post">
+                <!--BackUp View hier einfügen-->
+                <div class="scroll-view">
+                    <table class="table table-striped table-dark table-hover table-sm rounded" id="backup_tab">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Log Datum</th>
+                                <th>Auswahl</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $backupPath = "/home/dotto/backup/";
+                            $dataInPath = scandir($backupPath);
+                            $dataInPath = array_reverse($dataInPath);
+                            $iter = 1;
+                            foreach ($dataInPath as $data) {
+                                if ($data == "." || $data == ".." || $data == "backup.sh") {
+                                    continue;
+                                }
+                                echo "<tr id='row " . $iter . "'>
                             <td>$iter</td>
                             <td>$data</td>
-                            <td ><input style='margin-left: 25px;' type='checkbox'/></td>
+                            <td>
+                            <input style='margin-left: 25px;' type='checkbox' name=\"auswahl[]\" value=\"$data\"/>
+                            </td>
                             </tr>";
-                            $iter = $iter + 1;
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-            <div class="button-container">
-                <form action="admin_view.php" method="post">
-
-                    <input type="submit" value="Backup erstellen" name="btn_Submit" class="btn btn-success" style="margin-left: 1em; margin-bottom: 1em;" />
-                    <input type="button" value="Backup Vorschau" onclick="GetSelected()" name="btn_Submit" class="btn btn-primary" style="margin-left: 1em; margin-bottom: 1em;" />
-                    <input type="submit" value="Log laden" name="btn_Submit" class="btn btn-warning" style="margin-left: 1em; margin-bottom: 1em;" />
-
-                </form>
-            </div>
-        </div>
-        <div class="rightDiv">
-            <div style="margin: 5px;">
-                <h1 style="margin: 5px;">Vorschau</h1>
-                <div class="scroll-view" style="margin: 3em; background-color: grey;">
-                    <?php
-                    if (isset($_POST['btn_Submit'])) {
-                        shell_exec('/bin/bash /home/dotto/backup/backup.sh');
-                        echo '<script>alert("Backup erstellt!");</script>';
-                    }
-                    ?>
+                                $iter = $iter + 1;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
+                <div class="button-container">
+                    <input type="submit" value="Backup erstellen" name="btn_bErstellen" class="btn btn-success" style="margin-left: 1em; margin-bottom: 1em;" />
+                    <input type="submit" value="Backup Vorschau" name="btn_bVorschau" class="btn btn-primary" style="margin-left: 1em; margin-bottom: 1em;" />
+                    <input type="submit" value="Log laden" name="btn_logLaden" class="btn btn-warning" style="margin-left: 1em; margin-bottom: 1em;" />
+            </form>
+        </div>
+    </div>
+    <div class="rightDiv">
+        <div style="margin: 5px;">
+            <h2 style="margin-left: 5px;">Vorschau</h2>
+            <div class="scroll-view" style="margin:1em; background-color: grey;">
+                <?php
+                if (isset($_POST['btn_bErstellen'])) {
+                    shell_exec('/bin/bash /home/dotto/backup/backup.sh');
+                    echo '<script>alert("Backup erstellt!");</script>';
+                }
+                if (isset($_POST['btn_bVorschau'])) {
+
+                    foreach ($_POST['auswahl'] as $item) {
+                        if (!empty($item)) {
+                            echo "<h3>Backup vom: " . substr($item, 16, 14) . "</h3><br><br>";
+                            echo file_get_contents("/home/dotto/backup/" . $item);
+                            return;
+                        }
+                    }
+                }
+                if (isset($_POST['btn_logLaden'])) {
+                    echo "<h3>Log: </h3><br><br>";
+                    echo file_get_contents("/home/dotto/log/log.txt");
+                }
+                ?>
             </div>
         </div>
+    </div>
     </div>
 </body>
 
