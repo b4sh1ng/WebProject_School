238fbee3a860c2d3618c32e2fc5b0501794b687b
b4sh1ng <96059339+b4sh1ng@users.noreply.github.com>
2023-03-22 11:23:23 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$log = fopen("../log/log.txt", "a") or die("Unable to open file!");
-$insert_log = "[" . date('Y-m-d H:i:s') . "]" . "Erfolgreicher Logout von: "  .  $_SESSION['data'] . "\r";
-fwrite($log, $insert_log);
-fclose($log);
-session_destroy();
-echo "<script> location.replace(\"index.php\"); </script>";
